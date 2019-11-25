695e9841df3e6b06227a49b1d2ae7de67e9b0373
Stanislav <akhmadeevst@gmail.com>
2019-11-25 15:04:57 +0400

    feat: add content news

---
+++ src/components/Main/Content/News/News.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './News.module.css';
+import Avatar from './img/avatar.jpg';
+
+const News = (props) => {
+  return (
+      <div className={s.content__news}>
+        <img src={Avatar} className={s.content__img}/>
+        <div className={s.content__block}>
+          <div className={s.content__h2}>
+            <h2><a href="#" className={s.content__link}>{props.h2}</a></h2>
+          </div>
+          <div className={s.content__text}>
+            <p>{props.entry}</p>
+          </div>
+          <div className={s.content__link_a}>
+            <a href="#" className={s.content__link_a_a}>Подробнее</a>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default News;
