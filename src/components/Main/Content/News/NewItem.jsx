aa211acc0e45a3434ccddb839820614754e5c4aa
Stanislav <akhmadeevst@gmail.com>
2019-11-27 12:22:10 +0400

    feat: change props NewItem

--- src/components/Main/Content/News/NewItem.jsx
+++ src/components/Main/Content/News/NewItem.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import s from './New.module.css';
+import s from './NewItem.module.css';
 import Avatar from './img/avatar.jpg';
+import {NavLink} from 'react-router-dom';
 
-const New = (props) => {
+const NewItem = (props) => {
+
+  let path = '/news/' + props.id;
+
   return (
       <div className={s.content__news}>
         <img src={Avatar} className={s.content__img}/>
         <div className={s.content__block}>
           <div className={s.content__h2}>
-            <h2><a href="#" className={s.content__link}>{props.h2}</a></h2>
+            <h2><NavLink to={path} className={s.content__link}>{props.h2}</NavLink></h2>
           </div>
           <div className={s.content__text}>
             <p>{props.entry}</p>
@@ -19,6 +23,6 @@ const New = (props) => {
         </div>
       </div>
   );
-}
+};
 
-export default New;
+export default NewItem;
