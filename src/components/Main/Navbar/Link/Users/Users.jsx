c32197fb720e3c46b284f02397289ebb8706b0a3
Stanislav <akhmadeevst@gmail.com>
2019-12-02 22:03:06 +0400

    feat: add class Users

--- src/components/Main/Navbar/Link/Users/Users.jsx
+++ src/components/Main/Navbar/Link/Users/Users.jsx
@@ -3,31 +3,30 @@ import styles from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../../../../assets/images/user.png';  
 
-let Users = (props) => {
-
-    let getUsers = () => 
-    {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response => {
-                    props.setUsers(response.data.items);
-                });
-        }
+class Users extends React.Component {
+    constructor() {
+        super();
+    }
+    
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                    this.props.setUsers(response.data.items);
+            });
     }
 
-    return (
-        <div>
-            <button onClick={getUsers}>Получить пользователя</button>
+    render() {
+        return <div>
             {
-            props.users.map(u => <div key={u.id}>
+            this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed 
-                            ? <button onClick={ () => { props.unFollow(u.id) } }>UnFollow</button> 
-                            : <button onClick={ () => { props.follow(u.id) } }>Follow</button>}
+                            ? <button onClick={ () => { this.props.unFollow(u.id) } }>UnFollow</button> 
+                            : <button onClick={ () => { this.props.follow(u.id) } }>Follow</button>}
                     </div>
                 </span>
                 <span>
@@ -43,7 +42,7 @@ let Users = (props) => {
             </div>)
             }
         </div>
-    );
-};
+    }
+}
 
 export default Users;
