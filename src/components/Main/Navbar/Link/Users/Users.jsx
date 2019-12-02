3674a26fa543a6113dc26cad6e33698e10c47d65
Stanislav <akhmadeevst@gmail.com>
2019-12-02 21:32:08 +0400

    feat: start API Users

--- src/components/Main/Navbar/Link/Users/Users.jsx
+++ src/components/Main/Navbar/Link/Users/Users.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../../../../assets/images/user.png';  
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1, photoUrl: 'http://kto.delovoysaratov.ru/wp-content/uploads/sites/2/2015/06/%D0%B7%D1%83%D0%B5%D0%B21.jpg', followed: false, fullName: 'Зуев Александр Пулатович', status: 'Национальная политика это все', location: {city: 'Saratov'}},
-            {id: 2, photoUrl: 'http://kto.delovoysaratov.ru/wp-content/uploads/sites/2/2015/06/%D0%B7%D1%83%D0%B5%D0%B21.jpg', followed: true, fullName: 'Мякшев Анатолий Павлович', status: 'Национальную политику надо менять', location: {city: 'Saratov'}},
-            {id: 3, photoUrl: 'http://kto.delovoysaratov.ru/wp-content/uploads/sites/2/2015/06/%D0%B7%D1%83%D0%B5%D0%B21.jpg', followed: false, fullName: 'Семенов Сергей Валерьевич', status: 'Национальную политику надо обдумать', location: {city: 'Saratov'}},
-        ])
+    let getUsers = () => 
+    {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
+
     return (
         <div>
+            <button onClick={getUsers}>Получить пользователя</button>
             {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
                     </div>
                     <div>
                         {u.followed 
@@ -26,11 +32,11 @@ let Users = (props) => {
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.city'}</div>
                         <div></div>
                     </span>
                 </span>
--- src/components/Main/Navbar/Navbar.jsx
+++ src/components/Main/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
         <ul>
           <li className={s.navbar__item}><NavLink to='/questions' className={s.navbar__link} activeClassName={s.active}>Вопрос-ответ</NavLink></li>
           <li className={s.navbar__item}><NavLink to="/persons" className={s.navbar__link} activeClassName={s.active}>Персоны</NavLink></li>
+          <li className={s.navbar__item}><NavLink to="/users" className={s.navbar__link} activeClassName={s.active}>Пользователи</NavLink></li>
           <li className={s.navbar__item}><NavLink to="/recommendations" className={s.navbar__link} activeClassName={s.active}>Рекомендации муниципальным органам власти, лидерам национально-культурных организаций</NavLink></li>
           <li className={s.navbar__item}><NavLink to="/law" className={s.navbar__link} activeClassName={s.active}>Законодательные акты. Комментарии. Правоприменительная практика</NavLink></li>
         </ul>
