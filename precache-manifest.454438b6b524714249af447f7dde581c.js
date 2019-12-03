e7cec6811f8e38c1428f2da657f41d0de6c0eaa8
Stanislav <akhmadeevst@gmail.com>
2019-12-03 21:28:29 +0400

    Updates

---
+++ precache-manifest.454438b6b524714249af447f7dde581c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a01cb1fd8dd69abf0eca5e63d9c48afb",
+    "url": "/saristoch/index.html"
+  },
+  {
+    "revision": "3c68a4c99ca2b9b60a45",
+    "url": "/saristoch/static/css/main.02e96edc.chunk.css"
+  },
+  {
+    "revision": "7c44bb18e740b4a20702",
+    "url": "/saristoch/static/js/2.c873d73f.chunk.js"
+  },
+  {
+    "revision": "3c68a4c99ca2b9b60a45",
+    "url": "/saristoch/static/js/main.03f7a4b4.chunk.js"
+  },
+  {
+    "revision": "1713ed19a69f25014d23",
+    "url": "/saristoch/static/js/runtime-main.9547ff75.js"
+  },
+  {
+    "revision": "1892a9b86511ae17dfb62234af648ca3",
+    "url": "/saristoch/static/media/MapOfSarObl.1892a9b8.svg"
+  },
+  {
+    "revision": "de6af0d19f3b978bdeb00f53b7393b13",
+    "url": "/saristoch/static/media/avatar.de6af0d1.jpg"
+  },
+  {
+    "revision": "81c038a8ca315a2035613898629900f5",
+    "url": "/saristoch/static/media/gerbSarObl.81c038a8.svg"
+  },
+  {
+    "revision": "8dd4e9fd43d5fea4d59a4f5e9d6a5a64",
+    "url": "/saristoch/static/media/grantLogo.8dd4e9fd.svg"
+  },
+  {
+    "revision": "2aaa3a78fb60ac6dc38fc1a9d035e3b9",
+    "url": "/saristoch/static/media/saristoch-fon.2aaa3a78.jpg"
+  },
+  {
+    "revision": "52b1b2de6adf8b13ce99def1d38a594f",
+    "url": "/saristoch/static/media/saristoch-logo.52b1b2de.svg"
+  },
+  {
+    "revision": "1897f97542abc7ed782d257ee781bc40",
+    "url": "/saristoch/static/media/user.1897f975.png"
+  }
+]);
